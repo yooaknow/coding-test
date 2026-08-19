152e80cdd8b34f0d7f1a117f9e3330a9519bc397
yooaknow <dbdkfma1228@naver.com>
2026-08-19 10:51:52 +0900

    [level 2] Title: 짝지어 제거하기, Time: 56.01 ms, Memory: 13.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,10 @@
+def solution(s):
+    stack = [] #스택 초기화
+    
+    for c in s:
+        if stack and stack[-1] ==c:
+            #스택이 비어있지 않고, 현재 문자와 스택의 맨 위 문자가 같으면, 
+            stack.pop() #스택의 맨 위 문자 제거
+        else:
+            stack.append(c) #스택에 현재 문자 추가
+    return int(not stack) #스택이 비어있으면 1, 그렇지 않으면 반환

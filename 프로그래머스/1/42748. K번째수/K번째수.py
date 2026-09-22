9ac1b6fbeb099dbfb1b3c8fc27b1d15bc6c3f5bf
yooaknow <dbdkfma1228@naver.com>
2026-09-22 21:35:09 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,11 +1,10 @@
 def solution(array, commands):
     answer = []
-    
-    for i in commands:
-
-        slice_array = array[i[0]-1: i[1]]
-        sort = sorted(slice_array)
-        answer.append(sort[i[2]-1])
+    for command in commands:
+        new_answer = sorted(array[command[0]-1:command[1]])
+        
+        answer.append(new_answer[command[2]-1])
+        print(command[2])
         
         
     return answer
